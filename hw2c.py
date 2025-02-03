06c10377e954ca4e8d139009e24baa1e796de055
fatimarojo17 <frojova@okstate.edu>
2025-02-03 14:02:36 -0600

    Add files via upload

--- hw2c.py
+++ hw2c.py
@@ -5,14 +5,15 @@ def main():
     '''
     Solve systems of linear equations using the Gauss-Seidel method
     Example 1: System of equations
-    4x + y + z = 7
-    x + 3y + 2z = 8
-    2x + y + 3z = 9
+    3x + y - z = 2
+    x + 4y + z = 12
+    2x + y + 2z = 10
 
     Example 2: System of equations
-    10x + 2y + 3z = 27
-    4x + 15y + 7z = 73
-    2x + y + 7z = 29
+    1x - 10y + 2z + 4w = 2
+    3x + 1y + 4z + 12w = 12
+    9x + 2y + 3z + 4w = 21
+    -1x + 2y + 7z + 3w = 37
     '''
     # Example 1: System of equations
     # 3x + y - z = 2
